2e68d4a1e9955b98de299284bf5f139ce89143c5
djaniAntova <djaniantova@gmail.com>
2023-11-03 11:26:27 +0200

    Create App.jsx

---
+++ 03.Intro to React and JSX/Lab/react-demo/src/App.jsx
@@ -0,0 +1,34 @@
+import Header from "./components/Header";
+import OurServices from "./components/OurServices";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import WhyUs from "./components/WhyUs";
+import Team from "./components/Team";
+import Client from "./components/Client";
+import Info from "./components/Info";
+
+function App() {
+  return (
+    <div>
+
+      <Header />
+ 
+      <OurServices />
+
+      <About />
+
+      <WhyUs />
+
+      <Team />
+
+      <Client />
+
+      <Info />
+
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
