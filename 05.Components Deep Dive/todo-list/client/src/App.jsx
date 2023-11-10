d42afd0e1c4860e89704e2fa5a85ae9b52a6df22
djaniAntova <djaniantova@gmail.com>
2023-11-10 13:53:02 +0200

    Create App.jsx

---
+++ 05.Components Deep Dive/todo-list/client/src/App.jsx
@@ -0,0 +1,25 @@
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import TodoList from "./components/TodoList"
+function App() {  
+
+  return (
+    <div>
+          {/* <!-- Navigation header --> */}
+    <Header />
+
+  {/* <!-- Main content --> */}
+  <main className="main">
+
+    <TodoList />
+
+  </main>
+
+  {/* <!-- Footer --> */}
+    <Footer />
+    </div>
+
+  )
+}
+
+export default App
