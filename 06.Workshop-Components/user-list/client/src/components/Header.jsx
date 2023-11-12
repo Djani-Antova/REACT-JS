9e081b0d3c3d8a2e805cac3e67c92bdcf5c21612
djaniAntova <djaniantova@gmail.com>
2023-11-12 16:54:33 +0200

    Create Header.jsx

---
+++ 06.Workshop-Components/user-list/client/src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <div>
+      <header className="header">
+        <div className="logo">
+          <span className="course">React Workshop - September 2023</span>
+        </div>
+      </header>
+    </div>
+  );
+}
